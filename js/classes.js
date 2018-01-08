b980d2432bad103925fcf276137e9851a8b56fe7
theo.basty <theo.basty@gmail.com>
2018-01-09 00:01:03 +0100

    implementing : - generate zombies automatically - move zombies - animate zombies - respect game phases - drawing zombies - count passing zombies - reduce zombies pv on click and add blinking effect

--- js/classes.js
+++ js/classes.js
@@ -4,22 +4,89 @@ class Zombie{
         this.posX = posX;
         this.posY = posY;
         this.animFrame = 0;
+        this.moveCycles = 0;
+        this.damageFrame = 0;
         switch (level) {
-            case 1:
+            case 0:
                 this.totalLife = 1;
                 break;
-            case 2:
+            case 1:
                 this.totalLife = 2;
                 break;
-            case 3:
+            case 2:
                 this.totalLife = 3;
                 break;
-            case 4:
+            case 3:
                 this.totalLife = 25;
                 break;
             default:
                 this.totalLife = 0;
         }
         this.life = this.totalLife;
+    }
+
+    drawOnCtx(contexte, spritesheet){
+        contexte.drawImage(spritesheet,
+            32*(this.level*2+this.damageFrame),          //sprite X
+            32*(this.animFrame),         //sprite Y
+            32,         //sprite width
+            32,         //sprite height
+            this.posX,  //display X
+            this.posY,  //display Y
+            64,         //display width
+            64          //display height
+        );
+        if(this.life === this.totalLife) {
+            contexte.fillStyle = "#004cff";
+        } else if(this.life > this.totalLife / 2){
+            contexte.fillStyle = "#ffcc00";
+        } else {
+            contexte.fillStyle = "#ff0000";
+        }
+        contexte.fillRect(this.posX, this.posY, 64*this.life/this.totalLife, 5);
+        contexte.strokeRect(this.posX, this.posY, 64, 5);
+    }
+
+    isHit(cvMouseX, cvMouseY){
+        let zbMouseX = cvMouseX - this.posX;
+        let zbMouseY = cvMouseY - this.posY;
+
+        switch(this.level){
+            case 0:
+                if(zbMouseX > 18 && zbMouseX <= 44 && zbMouseY >= 24 && zbMouseY <= 60){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            case 1:
+                if(zbMouseX > 16 && zbMouseX <= 48 && zbMouseY >= 22 && zbMouseY <= 58){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            case 2:
+                if(zbMouseX > 4 && zbMouseX <= 60 && zbMouseY >= 10 && zbMouseY <= 58){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            case 3:
+                if(zbMouseX > 8 && zbMouseX <= 54 && zbMouseY >= 4 && zbMouseY <= 58){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+        }
+    }
+
+    toString(){
+        return "x:" + this.posX + " "
+            + "y:" + this.posY + " "
+            + "lvl:" + this.level + " "
+            + "pv:" + this.life + " ";
     }
 }
--- js/game.js
+++ js/game.js
@@ -1,22 +1,163 @@
+let zombies = [];
+let canvas = document.getElementById("main-cv");
+let context = canvas.getContext("2d");
+let startTs = null;
+let gameTimeManagement = {
+    movePeriod: 100,
+    createPeriod: 2000,
+
+    lvl1TimeStamp: 30000/2,
+    lvl1Done: false,
+    lvl2TimeStamp: 100000/2,
+    lvl2Done: false,
+    bossTimeStamp: 140000/2,
+    bossDone: false,
+};
+let lvlMax = 0;
+
+let player = {
+    pv: 10,
+    score: 0,
+};
+
 let zombieSprites = new Image();
 zombieSprites.loaded = false;
-zombieSprites.src = "../ressources/spritesheet-dev.png";
+zombieSprites.src = "ressources/spritesheet-dev.png";
 zombieSprites.onload = function(){
     zombieSprites.loaded = true;
+    console.log("zombies sprites loaded");
+    render();
 };
 
 let backgroundSprites = new Image();
 backgroundSprites.loaded = false;
-backgroundSprites.src = "../ressources/spritesheet-dev.png";
+backgroundSprites.src = "ressources/spritesheet-dev.png";
 backgroundSprites.onload = function () {
     backgroundSprites.loaded = true;
+    console.log("background sprites loaded");
+    render();
 };
 
-let render = function(){
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+function render(){
     if(!backgroundSprites.loaded || !zombieSprites.loaded){
         return;
     }
+    context.clearRect(0, 0, 600, 800);
 
+    //TODO : Draw Background
+    // console.log("Rendering zombies :");
+    for(i = 0;i < zombies.length; i++){
+        zombies[i].drawOnCtx(context, zombieSprites);
+        // console.log("| " + zombies[i].toString());
+    }
+    // console.log("Done");
+}
 
+function moveZombies(){
+    for(i = 0;i<zombies.length;i++){
+        if(zombies[i].moveCycles >= zombies[i].level) {
+            zombies[i].moveCycles = 0;
+            zombies[i].posY += 5;
+            zombies[i].animFrame = (zombies[i].animFrame + 1) % 4;
+            if (zombies[i].posY >= 800 ){
+                player.pv -= 1;
+                zombies.splice(i, 1);
+                console.log("Ouch !!");
+            }
+        }
+        else {
+            zombies[i].moveCycles += 1;
+        }
+    }
+}
+
+function createZombies(boss = false){
+    let level = null;
+    if (boss){
+        level = 3
+    }
+    else {
+        level = getRandomIntInclusive(0, lvlMax);
+    }
+    let posX = getRandomIntInclusive(0, 536);
+    let posY = getRandomIntInclusive(0, 36);
+
+    zombies.push(new Zombie(level, posX, posY));
+}
+
+canvas.onclick = function(event){
+    let rect = this.getBoundingClientRect();
+    // console.log("clientX: " + (event.clientX - rect.x) + " - clientY: " + (event.clientY - rect.y));
+    for (i = zombies.length - 1; i >= 0; i--){
+        if(zombies[i].isHit(event.clientX - rect.x, event.clientY - rect.y)){
+            zombies[i].life -= 1;
+            if(zombies[i].life <= 0){
+                zombies.splice(i, 1);
+                player.score++;
+            }
+            else {
+                zombies[i].damageFrame = 1;
+                setTimeout(function (zombie) {
+                    zombie.damageFrame = 0;
+                }, 200, zombies[i]);
+            }
+            break;
+        }
+
+    }
 };
+
+function game(ts) {
+    if(startTs === null){
+        if(!backgroundSprites.loaded || !zombieSprites.loaded){
+            requestAnimationFrame(game);
+            return;
+        }
+        startTs = {
+            start: ts,
+            moveTs: ts,
+            createTs: ts,
+        };
+        console.log("START !!!");
+
+    }
+
+    if(ts - startTs.createTs >= gameTimeManagement.createPeriod){
+        startTs.createTs += gameTimeManagement.createPeriod;
+        createZombies();
+    }
+
+    if(ts - startTs.moveTs >= gameTimeManagement.movePeriod) {
+        startTs.moveTs += gameTimeManagement.movePeriod;
+        moveZombies();
+    }
+
+    if(!gameTimeManagement.lvl1Done && (ts - startTs.start >= gameTimeManagement.lvl1TimeStamp)){
+        console.log("lvl1 zombies incoming");
+        gameTimeManagement.lvl1Done = true;
+        lvlMax = 1;
+    }
+
+    if(!gameTimeManagement.lvl2Done && (ts - startTs.start >= gameTimeManagement.lvl2TimeStamp)){
+        console.log("lvl2 zombies incoming");
+        gameTimeManagement.lvl2Done = true;
+        lvlMax = 2;
+    }
+
+    if(!gameTimeManagement.bossDone && (ts - startTs.start >= gameTimeManagement.bossTimeStamp)){
+        console.log("boss incoming");
+        gameTimeManagement.bossDone = true;
+        createZombies(true);
+        gameTimeManagement.createPeriod = 1000;
+    }
+    render();
+    requestAnimationFrame(game);
+}
+requestAnimationFrame(game);
 
